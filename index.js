9eb58ca513f7b1e035b61a29a67630d9b82d3f8c
adrianetodesign <adrianetodesign@gmail.com>
2022-01-22 00:57:46 +0000

    Refactored commit to be a super function.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-let balance = 500.00;
-
 class Account {
 
   constructor(username) {
@@ -17,20 +15,24 @@ class transaction {
     this.account = account;
   }
 
+  commit() {
+    this.account.balance += this.value;
+  }
+
 }
 
 class Withdrawal extends transaction {
 
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return -this.amount;
   }
 
 };
 
 class Deposit extends transaction {
 
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
 
 };
@@ -43,16 +45,12 @@ class Deposit extends transaction {
 
 const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25, myAccount);
+t1 = new Deposit(120.00, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(50.25, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Transaction 1:', t2);
 
-t3 = new Deposit(120.00);
-t3.commit();
-console.log('Transaction 3:', t3);
-
-console.log('Balance:', balance);
+console.log('Balance:', myAccount.balance);
